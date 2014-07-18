5afc0c7b42507d88bd190eda1a1af5de65bb9649
Paritosh Botre <paritosh271089@gmail.com>
2014-07-18 22:28:37 +0530

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 SampleApp::Application.routes.draw do
   #get "users/new"
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   match '/home',  to: 'static_pages#home', via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'  
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   root 'static_pages#home'
   #match '/help',    to: 'static_pages#help',    via: 'get'
